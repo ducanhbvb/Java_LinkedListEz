e68039146085e305d908b67a7af49d8bae336ce8
ducanhbvb <ducanhnguyen.bvb@gmail.com>
2019-10-06 15:06:09 +0700

    6/10

---
+++ src/Power/TestMyLinkedList.java
@@ -0,0 +1,17 @@
+package Power;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList myLinkedList = new MyLinkedList(9);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+
+        myLinkedList.add(4,9);
+        myLinkedList.add(4,8);
+        myLinkedList.add(3,7);
+        myLinkedList.printList();
+
+    }
+}
